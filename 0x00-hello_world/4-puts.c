5f949310b6136a6a6235914a653c2b36d8b46f3a
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-16 09:52:07 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
-void main(void){
+/**
+ * main - use puts
+ * Description: print a string to the standard output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
