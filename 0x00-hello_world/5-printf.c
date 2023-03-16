eebf688752cd4ec544313e79d3c9feba8b370cd3
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-16 09:56:39 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Use printf
+ * Description: us printf to write on the standard output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
