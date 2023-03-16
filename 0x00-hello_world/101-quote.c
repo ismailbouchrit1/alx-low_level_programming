58b4b8f5b3d8a66c80c7069ab14f211af921b724
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-16 11:15:42 -0700

    Print without puts or printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Print a quote
+ * Description: Print a quote without printf or puts
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
