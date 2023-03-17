89aaefb5bb72ff2a49e32e38cda0e456a488c0a1
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-17 07:37:02 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Use only putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = a; c <= z; c++)
+		putshar(c);
+	putshar('\n');
+	return (0);
+}
