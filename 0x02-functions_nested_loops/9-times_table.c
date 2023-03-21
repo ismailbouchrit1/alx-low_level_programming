d01a3dadfdb5a643be3be248e0f08a8c5eefa3ce
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-21 16:49:44 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,17 +12,25 @@ void times_table(void)
 	{
 		for (m = 0; m < 10; m++)
 		{
-			p = m * n;
-			if (p / 10 == 0)
-				_putchar(p % 10  + '0');
-			else
+			p = n * m;
+			if (m == 0)
 			{
-				_putchar(p / 10 + '0');
-				_putchar(p % 10 + '0');
+				_putchar(p + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+
+			if (p < 10 && m != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(p + '0');
+			} else if (p >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
