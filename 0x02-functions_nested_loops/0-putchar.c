999600d9aedcac2f0cc3933ed91b73846593f149
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-21 08:13:18 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,18 @@
 
 /**
  * main - prints _putchar
- * return: 0
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
