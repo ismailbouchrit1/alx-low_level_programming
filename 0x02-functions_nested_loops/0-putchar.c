3e55da246ad087a5be2bfb533960192a938a1b2e
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-21 07:12:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<main.h>
+
+/**
+ * main - prints _putchar
+ * return: 0
+ */
+
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# PROJECT 0x02. C - Functions, nested loops
+#### ==> File: `0-putchar.c` is a program that prints `-putchar`, followed by a new line.
