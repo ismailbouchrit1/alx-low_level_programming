1168dd91372d5eae85d4f9a0da4aa2957fee80af
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-27 16:41:44 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * _strlen - String length
+ * @s: the string to calculate the length
+ * Return: strlen
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,9 @@
 #define FILE_MAIN
 
 #include<stdio.h>
-
+#include<string.h>
 int _putchar(char c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif
