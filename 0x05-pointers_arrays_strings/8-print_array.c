aedf28c7dc816b1d68ecb0fa4824c1efa9dd4956
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-27 18:18:39 -0700

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
-			printf("%d", *(str + i));
+			printf("%d", *(a + i));
 		else
-			printf(", %d", *(str + i));
+			printf(", %d", *(a + i));
 	}
-		printf('\n');
+		printf("\n");
 }
