35c51dbd991e096c04b25b62efb81f598136453a
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-30 07:09:42 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int i;
+
+	for (i = 0; src[i] != '\0')
+		dest[strlen(dest) + i] = src[i];
+	dest[strlen(dest) + i] = '\0';
+	return (dest);
 }
