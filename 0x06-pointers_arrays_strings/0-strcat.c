b16963c524b63cde5a4202f127982bceed282d1d
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-03-30 06:42:03 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: the destination string
+ * @src: the source string
+ * Return: a pointer of type char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+#include<stdio.h>
+#include<string.h>
+
+void _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+
+#endif
