0965906ac2a95652f19bc45edc2c84467dbd9e97
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-05 16:04:23 -0700

    _puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (!(*s))
+	if (*s == '\0')
 		_putchar('\n');
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
 }
