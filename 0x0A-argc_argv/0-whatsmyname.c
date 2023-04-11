6adcb14544de6c9907777255b5a8a0a585c9ff71
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-10 17:06:21 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * main - print the programme name
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+
+int _putchar(char c);
+
+
+#endif
