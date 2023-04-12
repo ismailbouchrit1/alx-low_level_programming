2db5fbd044cc88f24d7d4aced68c7a7b5d41eda2
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-11 18:04:37 -0700

    strcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,18 +22,22 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 
 	if (s1)
+	{
 		while (s1[i] != '\0')
 		{
 			string[i] = s1[i];
 			i++;
 		}
+	}
 	if (s2)
+	{
 		while (s2[j] != '\0')
 		{
 			string[i] = s2[j];
 			i++;
 			j++;
 		}
+	}
 	string[i] = '\0';
 
 	return (string);
