07a0ed20160a6d33f18692ec877f6439bdfe824b
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-11 18:03:23 -0700

    strcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,31 +14,26 @@ char *str_concat(char *s1, char *s2)
 	char *string;
 	unsigned int len, i, j;
 
-	if (s1 == NULL)
-		len = strlen(s2);
-	else if (s2 == NULL)
-		len = strlen(s1);
-	else if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	else
-		len = strlen(s1) + strlen(s2) + 1;
+	len = strlen(s1) + strlen(s2) + 1;
 	string = (char *) malloc(len * sizeof(char));
 	if (string == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
 
-	while (s1[i] != '\0')
-	{
-		string[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		string[i] = s2[j];
-		i++;
-		j++;
-	}
+	if (s1)
+		while (s1[i] != '\0')
+		{
+			string[i] = s1[i];
+			i++;
+		}
+	if (s2)
+		while (s2[j] != '\0')
+		{
+			string[i] = s2[j];
+			i++;
+			j++;
+		}
 	string[i] = '\0';
 
 	return (string);
