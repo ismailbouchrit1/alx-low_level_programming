23285370f697b24acfba4ca72f1e999def6ec1aa
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-17 18:15:22 -0700

    Print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include"dog.h"
+
+/**
+ * print_dog  - print dog infos
+ *
+ * @d: dog struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n", d->owner);
+	}
+}
