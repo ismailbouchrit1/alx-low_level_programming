adb728d814bb715b0502dd75842f7e63843da453
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-17 17:42:20 -0700

    structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
