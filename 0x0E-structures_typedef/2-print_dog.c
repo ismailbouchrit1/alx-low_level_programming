db2feed01b0ae64d9f6c44e7a911ac5ece4ca5ec
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-17 18:18:29 -0700

    Print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include<stdlib.h>
 #include"dog.h"
 
@@ -9,19 +10,13 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-		if (d->owner)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n", d->owner);
-	}
+	if (d == NULL)
+		return;
+
+	if (!(d->name))
+		d->name = "(nil)";
+	if (!(d->owner))
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
