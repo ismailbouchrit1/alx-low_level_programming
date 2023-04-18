22cb14244a576b7aa119c51efdd51276618b97d1
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-17 18:04:53 -0700

    initialize

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
  * struct dog - a dog's basic info
@@ -18,5 +17,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 
 #endif
