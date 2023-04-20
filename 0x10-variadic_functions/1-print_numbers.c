7e32294b9c067c0f8e542a7ac90359ed19f3bc5f
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-20 08:57:21 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,11 +18,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n ; i++)
 	{
 		if (!separator)
-			printf("%d", va_arg(list, int));
+			printf("%d", va_arg(ap, int));
 		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
+			printf("%d", va_arg(ap, int));
 		else
-			printf("%s%d", separator, va_arg(list, int));
+			printf("%s%d", separator, va_arg(ap, int));
 	}
 	va_end(ap);
 	printf("\n");
