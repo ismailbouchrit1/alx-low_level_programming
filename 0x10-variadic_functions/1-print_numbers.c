07da230539cd06d080aaf8a7fb84eecaef7264f8
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-20 08:48:09 -0700

    Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include"variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ *
+ * @separator: separator
+ * @n: number of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator)
+			printf("%s", separator);
+	}
+	printf("%d", va_arg(ap, int));
+	printf("\n");
+	va_end(ap);
+}
