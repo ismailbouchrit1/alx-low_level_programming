b25f532a0cf802e7186a5eb5d6659bdefc90f99d
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-19 17:58:41 -0700

    Function Pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_name -  prints a name.
+ *
+ * @name: the name to print
+ *
+ * @f: pointer to the function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,9 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
