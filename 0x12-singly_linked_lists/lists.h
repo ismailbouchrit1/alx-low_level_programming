5956e6579c0238e5bf81de777d4bdece2a0eb6cb
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-04-27 17:22:52 -0700

    Linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,22 @@
 #include <string.h>
 #include <stdio.h>
 
+
+/**
+ * struct list - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: pointer to the next node
+ *
+ * Description: singly linked list node
+ */
+typedef struct list
+{
+	char *str;
+	unsigned int len;
+	struct list *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 
 
