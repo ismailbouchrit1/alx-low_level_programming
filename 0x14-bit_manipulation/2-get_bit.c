2ea1082bc18f1a698482147f19f07006e6b9fddc
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-05-04 17:35:34 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - Entry point
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: int
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int h;
+
+	if (index > 64)
+		return (-1);
+
+	h = n >> index;
+
+	return (h & 1);
+}
