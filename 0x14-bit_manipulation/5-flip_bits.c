dbbcf346161cdd5a2c26b1a7e81d39d53ff5ede0
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-05-04 17:37:47 -0700

    Bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - Entry point
+ * @n: unsigned long int
+ * @m: unsigned long int
+ * Return: unsigned int
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int h;
+	int c = 0;
+
+	h = n ^ m;
+
+	while (h)
+	{
+		c++;
+		h &= (h - 1);
+	}
+
+	return (c);
+}
