75c170ce86ab21c02de91bdd9914a436dcd25b22
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-05-11 16:07:36 -0700

    Input Output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,17 +15,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+
 	o = open(filename, O_RDONLY);
 	if (o == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-		return (0);
 
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <elf.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
